6ac6795b5362156fbe290ecf8c0d363863809894
SusanGithaiga <susan.githaiga@student.moringaschool.com>
2022-12-12 08:52:28 +0300

    First commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Declare a function createEmployeeRecord and pass in  employee details as arguments.
 // Populate myObj keys with respective values.
 // Initialize timeInEvents and timeOutEvents to hold an empty array.
-function createEmployeeRecord([firstName, familyName, title, payPerHour, timeInEvents, timeOutEvents]){
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
     const myObj={
         firstName: firstName,
         familyName: familyName,
@@ -13,9 +13,10 @@ function createEmployeeRecord([firstName, familyName, title, payPerHour, timeInE
     }
     return myObj
 }
- function createEmployeeRecords(){
-//     const myEmployeeRecords = {...createEmployeeRecord};
- }
+ function createEmployeeRecords(newCreatedEmployeeRecords){
+    return newCreatedEmployeeRecords.map(createEmployeeRecord)
+}
+ 
  function  createTimeInEvent(myDates){
     // let myDateRecords= {
     //     type: 'TimeIn',
@@ -24,8 +25,37 @@ function createEmployeeRecord([firstName, familyName, title, payPerHour, timeInE
     // }
 
  }
+function createTimeOutEvent(){
 
- 
+}
+function hoursWorkedOnDate(){
+    let entryTime
+    let exitTime
+    for(let event of this.entryTimeEvents){
+        if (dateOfWork === event['date']){
+            entryTime = event['hour']
+        }
+        for( let event of this.exitTimeEvents){
+            if(dateOfWork === event['date']){
+                exitTime = event['hour']
+            }
+        }
+    }
+    return (exitTime - entryTime)
+}
+function wagesEarnedOnDate(){
+
+}
+// function allWagesFor(){
+
+// }
+function findEmployeeByFirstName(){
+
+}
+function calculatePayroll(){
+
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
